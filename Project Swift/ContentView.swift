6d3aed45d5cc95a2c4674fc5e4db10f13d889f12
Joshua Rashtian <joshrashtian1@gmail.com>
2023-11-14 23:44:31 -0800

    Initial Commit

---
+++ Project Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project Swift
+//
+//  Created by Joshua Rashtian on 11/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project Swift/Project_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_SwiftApp.swift
+//  Project Swift
+//
+//  Created by Joshua Rashtian on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
